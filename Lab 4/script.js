101f34c779cb48d4abf5179194f5d135d16025cc
LucasLealAI <lucas2006leal@gmail.com>
2025-09-18 17:06:47 -0300

    Cozinhe lil bro

---
+++ Lab 4/script.js
@@ -0,0 +1,16 @@
+function imprimeGuess(certo){
+    let guess = document.getElementById("numero").value;
+
+    if(guess < certo){
+        document.getElementById("resposta").innerHTML = errado1
+
+    } else if(guess > certo) {
+        document.getElementById("resposta").innerHTML = errado2
+    } else if(guess = certo) {
+        document.getElementById("resposta").innerHTML = errado3
+    }
+}
+
+function misterioso(){
+    const certo = parseInt(Math.random() * 100);
+}
